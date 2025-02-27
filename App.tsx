c628eb0fc61b47971cac770c5980cf4135996d34
Rodrigo Ibarra Sanchez <rodrigoibarrasanchez@gmail.com>
2025-02-27 14:12:38 -0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MobileAds, { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import {
+  getTrackingPermissionsAsync,
+  PermissionStatus,
+  requestTrackingPermissionsAsync,
+} from 'expo-tracking-transparency';
 
 export default function App() {
+
+  useEffect(() => {
+    const requestPermissions = async () => {
+      const { status } = await getTrackingPermissionsAsync();
+      if (status === PermissionStatus.UNDETERMINED) {
+        await requestTrackingPermissionsAsync();
+      }
+    };
+
+    requestPermissions();
+
+    // Inicializar Mobile Ads SDK
+    MobileAds()
+      .initialize()
+      .then(adapterStatuses => {
+        // La inicialización se completó
+        console.log('Initialization complete:', adapterStatuses);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.BANNER} />
     </View>
   );
 }
---
+++ ios/EmptySwift.swift
@@ -0,0 +1,8 @@
+//
+//  EmptySwift.swift
+//  admob
+//
+//  Created by Rodrigo Ibarra on 27/02/25.
+//
+
+import Foundation
